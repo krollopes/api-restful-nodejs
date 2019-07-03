85014b04c70d7f6ce990a92b8fead3ca5a94a49c
Caroline Lopes <kaka201155@gmail.com>
2019-07-03 13:56:29 -0300

    Subindo mini-projeto API Restful com node

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = require('./config/app_config');
+var db = require('./config/db_config');
+var User = require('./models/Produto');
+var produtoController = require('./controllers/produtoController');
+var produtos = require('./routes/produtoRouter');
+
+app.get('/', function(req, res) {
+    res.end('Bem-vindo a API de Produtos');
+});
+
+// rotas de produtos
+app.use('/produtos', produtos);
+
+
+
+
